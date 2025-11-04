0d8cea7ae36eac8676b4814b8133437ac4db4f14
favourhaya <favouryahaya102@gmail.com>
2025-11-04 00:39:57 +0000

    done java

--- JavaFa/javaFa.java
+++ JavaFa/javaFa.java
@@ -1,18 +1,19 @@
-package javaVersion;
+package JavaFa;
 
 import java.util.*;
 
 /*
 Clean up b4 we create github
  */
-public class javaFa {
-    
+
+    //Interface class for any fsa , both nfa and dfa,
     abstract class Fa {
         Map<Integer, State> states;
         HashSet<Character> alphabet;
         ArrayList<Transition> transition;  //change to transitions
         State startState;
-        Map<Integer, Map<Character, HashSet<Integer>>> transitionTable;
+        Map<Integer, Map<Character, HashSet<Integer>>> transitionTable; //eg,  q1: 'a':[q2,q3], this isnt directly what happens for a dfa b
+        //ut in theory it can work as it would just be q1: 'a': [q2] - just pointing to one value in the set
 
         public Fa() {
             states = new HashMap<>();
@@ -23,7 +24,7 @@ public class javaFa {
 
         public void addState(State state) {
             if (state.checkStart()) {
-                startState = state;
+                startState = state; 
             }
             
             states.put(state.getId(), state);
@@ -31,8 +32,8 @@ public class javaFa {
                 transitionTable.put(state.getId(), new HashMap<>());
         }
 
-        abstract void addTransition(int to, int from, char arg);
-        abstract Boolean accepts(String input);
+        abstract void addTransition(int to, int from, char arg);  //assuming these will be implemented differently depending on the fa
+        abstract Boolean accepts(String input); 
     }
 
     class State {
@@ -75,14 +76,14 @@ public class javaFa {
         public Character getArg() { return arg; }
     }
 
-    class Nfa extends Fa {
+    class Nfa extends Fa { //Nfa class that implements these features
         
         @Override
         void addTransition(int to, int from, char arg) {
             State toState = states.get(to);
             State fromState = states.get(from);
             
-            alphabet.add(arg);
+            alphabet.add(arg); 
             transition.add(new Transition(toState, fromState, arg));
         
             if (!transitionTable.containsKey(from)) {
@@ -93,7 +94,7 @@ public class javaFa {
                 transitionTable.get(from).put(arg, new HashSet<>());
             }
             
-            transitionTable.get(from).get(arg).add(to);
+            transitionTable.get(from).get(arg).add(to); 
         }
 
      
@@ -111,8 +112,8 @@ public class javaFa {
                 
                 // c epsilon transitions  = (\0)
                 if (transitionTable.get(current) != null) {
-                    if (transitionTable.get(current).containsKey('\0')) {
-                        for (Integer next : transitionTable.get(current).get('\0')) {
+                    if (transitionTable.get(current).containsKey('\0')) { 
+                        for (Integer next : transitionTable.get(current).get('\0')) { // adding all possible states that can be travelled to with ep closer
                             if (!visited.contains(next)) {
                                 stack.push(next);
                             }
@@ -122,6 +123,8 @@ public class javaFa {
             }
             return visited;
         }
+
+
 
         private HashSet<Integer> getNext(Integer stateId, char symbol) {
             HashSet<Integer> result = new HashSet<>();
@@ -135,7 +138,7 @@ public class javaFa {
             return result;
         }
 
-        Boolean isDeterministic() {
+        Boolean isDeterministic() { // looking for if it sees epsilon closure or multiple transitions from one state
             
             for (Integer stateId : states.keySet()) {
                 Map<Character, HashSet<Integer>> trans = transitionTable.get(stateId);
@@ -159,13 +162,13 @@ public class javaFa {
 
         Nfa convertToDFA() {
             Nfa dfa = new Nfa();
-            Map<HashSet<Integer>, Integer> stateMap = new HashMap<>();
-            ArrayList<HashSet<Integer>> worklist = new ArrayList<>();
-            int counter = 0;
+            Map<HashSet<Integer>, Integer> stateMap = new HashMap<>(); // like transition table but specifcally for dfa, transition table in interface can still also work for dfa
+            ArrayList<HashSet<Integer>> worklist = new ArrayList<>(); // what states still need to be worked on
+            int count = 0; // new ids
             
             HashSet<Integer> startSet = epsilonClosure(startState.getId());
             worklist.add(startSet);
-            stateMap.put(startSet, counter);
+            stateMap.put(startSet, count);
             
        
           
@@ -177,17 +180,17 @@ public class javaFa {
                 }
             }
             
-            dfa.addState(new State(counter, true, startAccepting));
-            counter++;
+            dfa.addState(new State(count, true, startAccepting));
+            count++;
             
-            int index = 0;
-            while (index < worklist.size()) {
-                HashSet<Integer> currentSet = worklist.get(index);
-                index++;
+            int i = 0;
+            while (i < worklist.size()) {
+                HashSet<Integer> currentSet = worklist.get(i);
+                i++;
                 
                 
                 for (Character c : alphabet) {
-                    if (c == '\0') continue;  
+                    if (c == '\0') continue;  //skips all eps
                     
                     HashSet<Integer> nextSet = new HashSet<>();
                     
@@ -202,13 +205,13 @@ public class javaFa {
                     
                     
                     HashSet<Integer> closedSet = new HashSet<>();
-                    for (Integer stateId : nextSet) {
+                    for (Integer stateId : nextSet) { // all states possible to reach from states which are next
                         HashSet<Integer> closure = epsilonClosure(stateId);
                         closedSet.addAll(closure);
                     }
                     
                     if (!stateMap.containsKey(closedSet)) {
-                        stateMap.put(closedSet, counter);
+                        stateMap.put(closedSet, count);
                         worklist.add(closedSet);
                         boolean accepting = false;
                         for (Integer id : closedSet) {
@@ -218,8 +221,8 @@ public class javaFa {
                             }
                         }
                         
-                        dfa.addState(new State(counter, false, accepting));
-                        counter++;
+                        dfa.addState(new State(count, false, accepting));
+                        count++;
                     }
                     
  
@@ -233,7 +236,7 @@ public class javaFa {
         }
 
         @Override
-        Boolean accepts(String input) {
+        Boolean accepts(String input) { //added accepts last as other funcitons example epsilon closure and next make the implementation far easier
    
             HashSet<Integer> currentStates = epsilonClosure(startState.getId());
             
@@ -272,5 +275,33 @@ public class javaFa {
             
             return false;
         }
+    }
+
+public class javaFa {
+    public static void main(String[] args) {
+
+        Nfa nfa = new Nfa();
+        
+        nfa.addState(new State(0, true, false));   // start
+        nfa.addState(new State(1, false, true));   // accept
+
+        nfa.addTransition(1, 0, 'a');
+        
+        // Test 1
+        System.out.println("'a': " + nfa.accepts("a"));   // true
+        System.out.println("'b': " + nfa.accepts("b"));   // false
+        
+        
+        // Test 2
+        Nfa nfa2 = new Nfa();
+        nfa2.addState(new State(0, true, false));
+        nfa2.addState(new State(1, false, true));
+        nfa2.addTransition(0, 0, 'x');  
+        nfa2.addTransition(1, 0, 'x'); 
+        
+        System.out.println("\nBefore: " + nfa2.isDeterministic());  // false
+        Nfa dfa = nfa2.convertToDFA();
+        System.out.println("After: " + dfa.isDeterministic());       // true
+        System.out.println("'x': " + dfa.accepts("x"));              // true
     }
 }
